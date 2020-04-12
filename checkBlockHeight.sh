d45f58feec873e16050f74a9d2cdcf2c7dbe9204
mach-diamond <pserafin96@gmail.com>
2020-04-11 23:32:47 -0400

    Restore truncated lines

--- checkBlockHeight.sh
+++ checkBlockHeight.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Obtain Local Node Block Height
-myLastBlockHeight="$(jcli rest v0 node stats get -h "http://127.0.0.1:3100/api" | grep -o "lastBlock$
+# Obtain Local Node Block HeightmyLastBlockHeight="$(jcli rest v0 node stats get -h "http://127.0.0.1:3100/api" | grep -o "lastBlockHeight: \"[[:digit:]]\{1,64\}\"")"# echo "$myLastBlockHeight"
 blockHeightNode=$(echo $myLastBlockHeight | cut -d '"' -f 2)
 # echo "$blockHeightNode"
-blockHeightNode=""
 
-if [[ $blockHeightNode ~= ^[0-9]+$ ]]
+if [[ $(echo $blockHeightNode | grep -E "^[[:digit:]]{1,}$") ]]
 then
+
    # Obtain True Block Height from poolTool.io api
-   trueBlockHeight=$(curl -s 'https://pooltool.s3-us-west-2.amazonaws.com/stats/heights.json' | pyth$
+   trueBlockHeight=$(curl -s 'https://pooltool.s3-us-west-2.amazonaws.com/stats/heights.json' | python -mjson.tool | grep -o "\"majoritymax\": [[:digit:]]\{1,64\}" )
+   # echo "$trueBlockHeight"
+
    blockHeightTruth=$(echo $trueBlockHeight | grep -o "[[:digit:]]\{1,64\}")
    # echo "$blockHeightTruth"
 
