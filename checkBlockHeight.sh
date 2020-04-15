bdb7ebabb1f5a3a3bcab9a9ef385885aec3a24f5
mach-diamond <pserafin96@gmail.com>
2020-04-14 22:20:44 -0400

    Update checkBlockHeight.sh

--- checkBlockHeight.sh
+++ checkBlockHeight.sh
@@ -1,30 +1,37 @@
-#!/bin/bash
+#!/usr/bin/env bash
+HOME=/root
+LOGNAME=root
+PWD=/home/pool-user/
+SHELL=/bin/bash
+LANG=en_US.UTF-8
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+maxDelta=24
+
+# THIS SHOULD TAKE CARE OF JCLI PATH (ASSUMES $PATH ; /usr/local/bin ; or same directory)
+JCLI="$(command -v jcli)"
+[ -z "$JCLI" ] && JCLI="/usr/local/bin/jcli"
+[ -z "$JCLI" ] && [ -f ./jcli ] && JCLI="./jcli"
 
 # Obtain Local Node Block Height
-myLastBlockHeight="$(jcli rest v0 node stats get -h "http://127.0.0.1:3100/api" | grep -o "lastBlockHeight: \"[[:digit:]]\{1,64\}\"")"
-# echo "$myLastBlockHeight"
+myLastBlockHeight=$($JCLI rest v0 node stats get -h "http://127.0.0.1:3100/api" | grep -o "lastBlockHeight: \"[[:digit:]]\{1,64\}\"")
 blockHeightNode=$(echo $myLastBlockHeight | cut -d '"' -f 2)
-# echo "$blockHeightNode"
 
 if [[ $(echo $blockHeightNode | grep -E "^[[:digit:]]{1,}$") ]]
 then
 
    # Obtain True Block Height from poolTool.io api
    trueBlockHeight=$(curl -s 'https://pooltool.s3-us-west-2.amazonaws.com/stats/heights.json' | python -mjson.tool | grep -o "\"majoritymax\": [[:digit:]]\{1,64\}" )
-   # echo "$trueBlockHeight"
-
    blockHeightTruth=$(echo $trueBlockHeight | grep -o "[[:digit:]]\{1,64\}")
-   # echo "$blockHeightTruth"
 
    # Compare Block Heights
    myBlockLag=$(( blockHeightTruth - blockHeightNode ))
-   # echo $myBlockLag
 
    # If Block Height is Frozen, Restart the Node
-   if [ $myBlockLag -gt 50 ]
+   if [ $myBlockLag -gt $maxDelta ]
    then
-      echo "Restarting Stake Pool Node"
-      echo "Block Divergence: $myBlockLag"
-      systemctl restart jormungandr.service
+      # echo "Restarting Node: $restartTime" >> /home/pool-user/blockDivergence.txt
+      echo "Block Divergence: $myBlockLag" >> /home/pool-user/blockDivergence.txt
+      /bin/systemctl restart jormungandr.service
    fi
 fi
